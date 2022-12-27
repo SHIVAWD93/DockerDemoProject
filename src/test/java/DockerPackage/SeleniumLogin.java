64e37154fc038be3d57b518eab50521ea22ec941
shivakumar.alamanda <shivakumar.alamanda@indhydlpt-0281>
2022-12-27 17:45:31 +0530

    webdriver

--- src/test/java/DockerPackage/SeleniumLogin.java
+++ src/test/java/DockerPackage/SeleniumLogin.java
@@ -3,14 +3,21 @@ package DockerPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumLogin {
 
 	public static void main(String[] args)
 	{
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver");
+		//System.setProperty("webdriver.chrome.driver","./chromedriver");
 		
-		WebDriver driver = new ChromeDriver();
+		
+		ChromeOptions co =new ChromeOptions();
+		co.setHeadless(false);
+		
+		WebDriver driver = WebDriverManager.chromedriver().capabilities(co).create();
 		// URL of the login website that is tested
 		driver.get("https://auth.geeksforgeeks.org/");
 		// Maximize window size of browser
